79a080022bc6dd61fe34ae1c8a1cb926094ea17c
mark <markferdinan.mfa@gmail.com>
2023-05-20 09:02:51 +0800

    added user public profile

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,34 +1,32 @@
 import NextAuth from "next-auth";
-import GoggleProvider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
   providers: [
-    GoggleProvider({
+    GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   callbacks: {
     async session({ session }) {
-      const sessionUser = await User.findOne({
-        email: session.user.email,
-      });
-
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
       session.user.id = sessionUser._id.toString();
 
       return session;
     },
-    async signIn({ profile }) {
+    async signIn({ account, profile, user, credentials }) {
       try {
         await connectToDB();
 
-        // check if a user is already exists
+        // check if user already exists
         const userExists = await User.findOne({ email: profile.email });
 
-        // if not, create a new user
+        // if not, create a new document and save user in MongoDB
         if (!userExists) {
           await User.create({
             email: profile.email,
@@ -39,7 +37,7 @@ const handler = NextAuth({
 
         return true;
       } catch (error) {
-        console.log(error);
+        console.log("Error checking if user exists: ", error.message);
         return false;
       }
     },
